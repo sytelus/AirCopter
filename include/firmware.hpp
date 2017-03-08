0683b48ac87c86d0de951e575da4f7408a0eee8d
Shital Shah <shital@ShitalShah.com>
2017-03-07 19:27:56 -0800

    converted to header-only library except for turbotrig and vec

--- include/firmware.hpp
+++ include/firmware.hpp
@@ -53,4 +53,77 @@ private:
     uint64_t imu_time;
 };
 
+
+/************************************************** Implementation ***************************************************************/
+
+Firmware::Firmware(Board* _board, CommLink* _comm_link)
+    : board(_board), comm_link(_comm_link)
+{
+}
+
+void Firmware::setup()
+{
+    board->init();
+
+    //initialize parameters source such as EPROM
+    params.init(board, comm_link);
+
+    // Initialize communication stack such as MavLink
+    comm_link->init();
+
+
+    // Initialize Estimator
+    // mat_exp <- greater accuracy, but adds ~90 us
+    // quadratic_integration <- some additional accuracy, adds ~20 us
+    // accelerometer correction <- if using angle mode, this is required, adds ~70 us
+    estimator.init(&params, false, false, true);
+
+    // Initialize Sensors
+    sensors.init(&common_state, board, &estimator, &params, comm_link);
+
+    mux.init(&common_state, board, &params);
+
+    // Initialize Motor Mixing
+    mixer.init(&common_state, board, &params);
+
+    controller.init(&common_state, board, &mux, &mixer, &estimator, &params, comm_link);
+
+    rc.init(&common_state, board, &mux, &params, comm_link);
+
+    mode.init(&common_state, &sensors, &rc, &params);
+
+}
+
+void Firmware::loop()
+{
+    /*********************/
+    /***  Control Loop ***/
+    /*********************/
+    if (sensors.update_sensors()) // 595 | 591 | 590 us
+    {
+        // If I have new IMU data, then perform control
+        sensors.get_imu_measurements(accel, gyro, imu_time);
+        estimator.run_estimator(accel, gyro, imu_time); //  212 | 195 us (acc and gyro only, not exp propagation no quadratic integration)
+        controller.run_controller(); // 278 | 271
+        mixer.mix_output(); // 16 | 13 us
+    }
+
+    /*********************/
+    /***  Post-Process ***/
+    /*********************/
+    //Let communication stack send and recieve messages
+    comm_link->update(); // 165 | 27 | 2
+
+                         // update the armed_states, an internal timer runs this at a fixed rate
+    mode.check_mode(board->micros()); // 108 | 1 | 1
+
+                                      // get RC, an internal timer runs this every 20 ms (50 Hz)
+    rc.receive_rc(board->micros()); // 42 | 2 | 1
+
+                                    // update commands (internal logic tells whether or not we should do anything or not)
+    mux.mux_inputs(); // 6 | 1 | 1
+}
+
+
+
 } //namespace
--- include/mode.hpp
+++ include/mode.hpp
@@ -40,4 +40,147 @@ private:
 };
 
 
+/************************************************** Implementation ***************************************************************/
+void Mode::init(CommonState* _common_state, Sensors* _sensors, RC* _rc, Params* _params)
+{
+    params = _params;
+    common_state = _common_state;
+    sensors = _sensors;
+    rc = _rc;
+
+    common_state->setToDisarm();
+}
+
+bool Mode::arm(void)
+{
+    if (!started_gyro_calibration && common_state->isDisarmed())
+    {
+        sensors->start_gyro_calibration();
+        started_gyro_calibration = true;
+        return false;
+    } else if (sensors->gyro_calibration_complete())
+    {
+        started_gyro_calibration = false;
+        common_state->setToArm();
+        board->setLed(0, true);
+        return true;
+    }
+    return false;
+}
+
+void Mode::disarm(void)
+{
+    common_state->setToDisarm();
+    board->setLed(0, true);
+}
+
+/// TODO: Be able to tell if the RC has become disconnected during flight
+bool Mode::check_failsafe(void)
+{
+    for (int8_t i = 0; i < params->get_param_int(Params::PARAM_RC_NUM_CHANNELS); i++)
+    {
+        if (board->pwmRead(i) < 900 || board->pwmRead(i) > 2100)
+        {
+            if (common_state->isArmed() || common_state->isDisarmed())
+            {
+                common_state->setArmedState(CommonState::FAILSAFE_DISARMED);
+            }
+
+            // blink LED
+            if (blink_count > 25)
+            {
+                board->toggleLed(1);
+                blink_count = 0;
+            }
+            blink_count++;
+            return true;
+        }
+    }
+
+    // we got a valid RC measurement for all channels
+    if (common_state->getArmedState() == CommonState::FAILSAFE_ARMED || common_state->getArmedState() == CommonState::FAILSAFE_DISARMED)
+    {
+        // return to appropriate mode
+        common_state->setArmedState(
+            (common_state->getArmedState() == CommonState::FAILSAFE_ARMED) ? CommonState::ARMED :CommonState::DISARMED
+        );
+    }
+    return false;
+}
+
+
+bool Mode::check_mode(uint64_t now)
+{
+    // see it has been at least 20 ms
+    uint32_t dt = static_cast<uint32_t>(now - prev_time);
+    if (dt < 20000)
+    {
+        return false;
+    }
+
+    // if it has, then do stuff
+    prev_time = now;
+
+    // check for failsafe mode
+    if (check_failsafe())
+    {
+        return true;
+    } else
+    {
+        // check for arming switch
+        if (params->get_param_int(Params::PARAM_ARM_STICKS))
+        {
+            if (common_state->getArmedState() == CommonState::DISARMED)
+            {
+                // if left stick is down and to the right
+                if (board->pwmRead(params->get_param_int(Params::PARAM_RC_F_CHANNEL)) < params->get_param_int(Params::PARAM_RC_F_BOTTOM) + params->get_param_int(Params::PARAM_ARM_THRESHOLD)
+                    && board->pwmRead(params->get_param_int(Params::PARAM_RC_Z_CHANNEL)) > (params->get_param_int(Params::PARAM_RC_Z_CENTER) + params->get_param_int(Params::PARAM_RC_Z_RANGE) / 2)
+                    - params->get_param_int(Params::PARAM_ARM_THRESHOLD))
+                {
+                    time_sticks_have_been_in_arming_position += dt;
+                } else
+                {
+                    time_sticks_have_been_in_arming_position = 0;
+                }
+                if (time_sticks_have_been_in_arming_position > 500000)
+                {
+                    if (arm())
+                        time_sticks_have_been_in_arming_position = 0;
+                }
+            } else // _armed_state is ARMED
+            {
+                // if left stick is down and to the left
+                if (board->pwmRead(params->get_param_int(Params::PARAM_RC_F_CHANNEL)) < params->get_param_int(Params::PARAM_RC_F_BOTTOM) +
+                    params->get_param_int(Params::PARAM_ARM_THRESHOLD)
+                    && board->pwmRead(params->get_param_int(Params::PARAM_RC_Z_CHANNEL)) < (params->get_param_int(Params::PARAM_RC_Z_CENTER) - params->get_param_int(Params::PARAM_RC_Z_RANGE) / 2)
+                    + params->get_param_int(Params::PARAM_ARM_THRESHOLD))
+                {
+                    time_sticks_have_been_in_arming_position += dt;
+                } else
+                {
+                    time_sticks_have_been_in_arming_position = 0;
+                }
+                if (time_sticks_have_been_in_arming_position > 500000)
+                {
+                    disarm();
+                    time_sticks_have_been_in_arming_position = 0;
+                }
+            }
+        } else
+        {
+            if (rc->rc_switch(params->get_param_int(Params::PARAM_ARM_CHANNEL)))
+            {
+                if (common_state->getArmedState() == CommonState::DISARMED)
+                    arm();
+            } else
+            {
+                disarm();
+            }
+        }
+    }
+    return true;
+}
+
+
+
 } //namespace
